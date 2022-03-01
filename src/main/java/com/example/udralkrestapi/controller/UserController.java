4abcd6887f91e4e322d9a5056f3906acd99698f1
udrathnayaka <rathnayakar07@gmail.com>
2022-03-01 19:56:42 +0530

    services

--- src/main/java/com/example/udralkrestapi/controller/UserController.java
+++ src/main/java/com/example/udralkrestapi/controller/UserController.java
@@ -5,31 +5,33 @@ import com.example.udralkrestapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/api/v1/users")
 @CrossOrigin
 public class UserController {
     @Autowired
-    private UserService userservice;
+    private UserService userService;
     
-    @GetMapping("/getUser")
-    public String getUser(){
-        return "Udralk";
+    @GetMapping("/getUsers")
+    public List<UserDTO> getUser(){
+        return userService.getAllUsers();
     }
 
     @PostMapping("/saveUser")
     public UserDTO saveUser(@RequestBody UserDTO userDTO){
-       return userservice.saveUser(userDTO);
+       return userService.saveUser(userDTO);
     }
 
     @PutMapping("/updateUser")
-    public String updateUser(){
-        return "User updated!";
+    public UserDTO updateUser(@RequestBody UserDTO userDTO){
+        return userService.updateUser(userDTO);
     }
 
     @DeleteMapping("/deleteUser")
-    public String deleteUser(){
-        return "User deleted!";
+    public boolean deleteUser(@RequestBody UserDTO userDTO){
+        return userService.deleteUser(userDTO);
     }
 
 }
--- src/main/java/com/example/udralkrestapi/service/UserService.java
+++ src/main/java/com/example/udralkrestapi/service/UserService.java
@@ -4,10 +4,12 @@ import com.example.udralkrestapi.dto.UserDTO;
 import com.example.udralkrestapi.entity.User;
 import com.example.udralkrestapi.repository.UserRepo;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @Transactional
@@ -15,10 +17,27 @@ public class UserService {
 
     @Autowired
     private UserRepo userRepo;
+
     @Autowired
     private ModelMapper modelMapper;
+
     public UserDTO saveUser(UserDTO userDTO){
         userRepo.save(modelMapper.map(userDTO, User.class));
         return userDTO;
+    }
+
+    public List<UserDTO> getAllUsers(){
+        List<User> userList = userRepo.findAll();
+        return modelMapper.map(userList, new TypeToken<List<UserDTO>>(){}.getType());
+    }
+
+    public UserDTO updateUser(UserDTO userDTO){
+        userRepo.save(modelMapper.map(userDTO, User.class));
+        return userDTO;
+    }
+
+    public boolean deleteUser(UserDTO userDTO){
+        userRepo.delete(modelMapper.map(userDTO, User.class));
+        return true;
     }
 }
